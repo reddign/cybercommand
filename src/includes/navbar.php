b0e6332d89b817cf9caad08b1a709830ff2aa3b4
olivyahvanek <[Lvanek1999@gmail.com]>
2022-09-22 08:59:04 -0400

    Getting rid of db title

--- src/includes/navbar.php
+++ src/includes/navbar.php
@@ -3,7 +3,7 @@
   <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
   <div class="w3-container">
    <img width="200px" src="<?php echo $path; ?>images/Etown_Black_&_White.png">
-    <h3 class="w3-padding-34"><b>EMCS Career DB</b></h3>
+   <!-- <h3 class="w3-padding-34"><b>EMCS Career DB</b></h3> -->
   </div>
   <div class="w3-bar-block">
     <a href="<?php echo $path; ?>index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a> 
