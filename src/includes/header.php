bcfac9c3639e1c8af6cabac5913a7d661ed5fb94
knappenbergerk <knappenbergerk@etown.edu>
2022-09-29 11:57:13 -0400

    edited navbar logo

--- src/includes/header.php
+++ src/includes/header.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins|Oswald">
 <link rel ="stylesheet" href="<?php echo $path; ?>css/style.css">
 </head>
 <?PHP
--- src/survey.php
+++ src/survey.php
@@ -9,8 +9,8 @@ require("functions/survey_functions.php");
  <!-- Header -->
  <div class="w3-container" style="margin-top:80px" id="showcase">
     <h1 class="w3-jumbo"><b>Survey</b></h1>
-    <h1 class="w3-xxxlarge w3-text-red"><b>Student Interest.</b></h1>
-    <hr style="width:50px;border:5px solid red" class="w3-round">
+    <h1 class="w3-xxxlarge w3-text- blue"><b>Student Interest.</b></h1>
+    <hr style="width:50px;border:5px solid blue" class="w3-round">
   </div>
 
 <?php
