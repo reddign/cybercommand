084e0a4e96c6cf7b4f70a72308e057413f681213
knappenbergerk <knappenbergerk@etown.edu>
2022-09-29 09:18:43 -0400

    Edited the home page

--- src/index.php
+++ src/index.php
@@ -4,22 +4,24 @@ require("functions/basic_html_functions.php");
 require("includes/header.php");
 display_small_page_heading("Engineering, Mathematics, & Computer Science Career Database");
 
-  echo "Welcome to the main page of the EMCS Database.";
+  echo "Welcome to the main page of the EMCS Database.<BR/>";
+  echo "The photos you see here are all from Elizabethtown College's Instagram.";
+
 
   ?>
   
   <!-- Photo grid (modal) -->
   <div class="w3-row-padding">
     <div class="w3-half">
-      <img src="images/student1.jfif" style="width:100%" onclick="onClick(this)" alt="CS Students in a Summer SCARP project">
-      <img src="images/student2.jpg" style="width:100%" onclick="onClick(this)" alt="Students using tech">
-      <img src="images/student3.png" style="width:100%" onclick="onClick(this)" alt="Students using tech">
+      <img src="images/Adam Geltz.jpg" style="width:100%" onclick="onClick(this)" alt="Adam Geltz ‘23 collaborated with a local Harrisburg startup this summer to help predict the remaining useful life of smart manufacturing equipment and proactively detect disruptive incidents. The ETown Data Science major is gaining an invaluable real-world learning experience in the process!" alt= https://www.instagram.com/p/CSPIkbHD1ba/>
+      <img src="images/Kyla DeWittie.jpg" style="width:100%" onclick="onClick(this)" alt="Kyla DeWittie '24 and Yudeliz Sanchez '25 worked together this summer to advance previous #EtownSCARP research that aims to develop a quick, inexpensive and sensitive paper-based test to detect low levels of lead in drinking water.">
+      <img src="images/Justin Brown.jpg" style="width:100%" onclick="onClick(this)" alt="Working in the Bollman Fabrication Lab is great because it is a place on campus where you can find advanced hands-on tools and technologies available for students to use for designing and creating anything they can come up with. Having access to the Lab increases the quality of my engineering education because this is the only place where I get to test and experience successes and failures. One of the best ways to learn is by doing, and the Lab is a quintessential example of a location that embraces that ideal. - Justin Brown '23, Engineering major with a concentration in Mechanical/Electrical">
     </div>
 
     <div class="w3-half">
-      <img src="images/industry1.jfif" style="width:100%" onclick="onClick(this)" alt="Phoenix Connect employee works in the lab">
-      <img src="images/industry2.jfif" style="width:100%" onclick="onClick(this)" alt="Company with an opportunity">
-      <img src="images/industry3.jpeg" style="width:100%" onclick="onClick(this)" alt="Instacarts first software intern">
+      <img src="images/Brianna Falby.jpg" style="width:100%" onclick="onClick(this)" alt="Etown has helped me to achieve experiences that enhanced my programming skills in real-world problems. My data analytics minor with my mathematics degree has helped me to reach a summer Research Experiences for Undergraduates (REU) internship with @childrensphila Center for Research and Prevention (CIRP). I have been able to use my programming skills to build data frames and run statistical tests to analyze differences in opinions on crash avoidance technologies. I appreciate the Math department faculty who have prepared me for this internship and a start to my career path. - Brianna Falby '22, Mathematics major, Data Analytics minor">
+      <img src="images/Nathan Griffin.jpg" style="width:100%" onclick="onClick(this)" alt="Computer Engineering major Nathan Griffin ’23 is expanding on his previous #EtownSCARP research by creating algorithms that allow prosthesis users to experience artificial touch and pressure feedback through electrical stimulation.">
+      <img src="images/Olivia Kurtz.jpg" style="width:100%" onclick="onClick(this)" alt="I'm pursuing Civil Engineering at #etowncollege because I want to make a lasting impact on the world around me. I enjoy the flexibility that comes with engineering design. Often there is more than one right answer to a problem. Etown is preparing me to be successful by challenging me with real-world problems applicable to my field. My favorite experience has been my internship at HBK Engineering, and after graduation, I would like to work for an engineering consultant company, obtain my engineer-in-training (EIT) certification, and eventually get my Professional Engineer (PE) license. - Olivia Kurtz '24">
     </div>
   </div>
 
